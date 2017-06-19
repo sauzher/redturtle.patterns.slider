390f65124f1923d7530bab633d67843ebb1ebd6a
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-06-19 08:24:35 +0200

    some refactoring

--- src/redturtle/patterns/slider/static/pattern.js
+++ src/redturtle/patterns/slider/static/pattern.js
@@ -1,24 +1,24 @@
 define('redturtle-patterns-slider', [
   'jquery',
-  'pat-base',
-  'slick.min'
-], function($, Base) {
+  'mockup-patterns-base',
+  'slick.min',
+], function ($, Base) {
   'use strict';
-
   var Slider = Base.extend({
     name: 'slider',
     trigger: '.pat-slider',
     parser: 'mockup',
     defaults: {
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
     },
-    init: function() {
-        var that = this;
-        that.$el.slick(that.defaults);
-    }
+    init: function () {
+      console.log('ciao!');
+      var that = this;
+      that.$el.slick(that.defaults);
+    },
   });
 
   return Slider;
